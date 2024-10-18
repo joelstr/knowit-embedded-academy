d352c21a3e271e40e36e07c7039001985e994419
Joel Strand <joel.strand@knowit.se>
2024-10-18 08:16:04 +0200

    Add simple function pointer example

--- Pre-course/pointers/pointers.c
+++ Pre-course/pointers/pointers.c
@@ -6,6 +6,7 @@ static void passing_pointer();
 static void passing_array();
 static void passing_null_pointer();
 static void increasing_pointer();
+static void simple_function_pointer();
 static void function_pointer();
 static void struct_pointer();
 static void pointer_to_pointer();
@@ -32,6 +33,7 @@ int main() {
     passing_array();
     // passing_null_pointer();
     increasing_pointer();
+    simple_function_pointer();
     function_pointer();
     struct_pointer();
     pointer_to_pointer();
@@ -46,7 +48,7 @@ static void inc_by_one(int32_t *x, int32_t * y)
 
 static void inc_by_two(struct coords *c)
 {
-    c->x = c->x + 2;
+    c->x = c->x + 2; // c->x is equivalent to (*c).x;
     c->y = c->y + 2;
 }
 
@@ -101,6 +103,16 @@ static void increasing_pointer()
     printf("First element: %d\n", *ptr);
     ptr = ptr + 2;
     printf("Third element: %d\n", *ptr);
+}
+
+static void simple_function_pointer()
+{
+    int32_t x = 10;
+    int32_t y = 20;
+    void (*func_ptr)(int32_t *, int32_t *) = inc_by_one;
+    func_ptr(&x, &y);
+    printf("x: %d\n", x);
+    printf("y: %d\n", y);
 }
 
 static void function_pointer()
